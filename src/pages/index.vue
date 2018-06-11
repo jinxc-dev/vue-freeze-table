5f24ee7925b44e228d2947c8c03bf8ce84f9b25d
timegold-websrc <39736627+timegold-websrc@users.noreply.github.com>
2018-06-12 06:21:02 +0800

    Fix table height

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,42 +5,56 @@
 			:data="tableData"
 			:columns="columns"
 			row-key="name"
-		/>
+			tableClass="fix-height"
+
+		>
+		
+		<!-- <template slot="top-middle" slot-scope="props" :class="tableClass">			
+		</template> -->
+		</q-table>
 	</q-page>
 </template>
 
 <style>
+	.fix-height {
+		height: 200px;
+	}
 </style>
 
 <script>
 // export default {
 // 	name: 'PageIndex'
 // }
+import tableData from 'assets/table-data'
 
 export default {
-  data: () => ({
-	columns: [
-	  {
-		name: 'desc',
-		required: true,
-		label: 'Dessert (100g serving)',
-		align: 'left',
-		field: 'name',
-		sortable: true
-	  },
-	],
-	tableData: [
-	  {
-		name: 'Frozen Yogurt',
-		calories: 159,
-		fat: 6.0,
-		carbs: 24,
-		protein: 4.0,
-		sodium: 87,
-		calcium: '14%',
-		iron: '1%'
-	  },
-	]
-  })
+	data() {
+		return {
+			tableData,
+			columns: [
+				{
+					name: 'desc',
+					required: true,
+					label: 'Dessert (100g serving)',
+					align: 'left',
+					field: 'name',
+					sortable: true
+				},
+				{ name: 'calories', label: 'Calories', field: 'calories', sortable: true },
+				{ name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
+				{ name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
+				{ name: 'protein', label: 'Protein (g)', field: 'protein' },
+				{ name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
+				{ name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
+				{ name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+			],
+		}
+	},
+	computed: {
+		tableClass () {
+			return 'fix-height'
+		}
+	},
+
 }
 </script>
